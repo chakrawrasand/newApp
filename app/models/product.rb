e52cd3453807ffe04d2d338bd7f28147540600de
chakrawrasand <chakras.campur@gmail.com>
2021-02-16 17:24:32 +0700

    2nd commit
    last commit 16feb

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,14 @@ class Product < ApplicationRecord
         greater_than_or_equal_to: 1000,
         message: "harus lebih dari 1000"
     }
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    private
+    def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+            errors.add(:base, 'line items present')
+            throw :abort
+        end
+    end
 end
