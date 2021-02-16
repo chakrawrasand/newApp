c37882da104591eef2ffcbd4ee94cce5a3b75d63
chakrawrasand <chakras.campur@gmail.com>
2021-02-16 14:17:41 +0700

    add

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,7 @@
 class Product < ApplicationRecord
     validates :nama, allow_blank: false, uniqueness:{message: "%{value} sudah terdaftar"}
     validates :harga, numericality: {
-        only_integer: true,
         greater_than_or_equal_to: 1000,
-        message: "harus lebih dari Rp. 1000"
+        message: "harus lebih dari 1000"
     }
 end
